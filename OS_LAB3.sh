8a00fbb6b0e0587260e14d079cc4d57b2d6a4a93
AdamKhalil27 <95482399+AdamKhalil27@users.noreply.github.com>
2023-09-13 15:23:24 +0200

    Add files via upload

---
+++ OS_LAB3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pwd
+nano my_script.sh
+ls
+chmod +x my_script.sh
+bash my_script.sh
+bash −x my_script.sh
+
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.10.0-windows-x86_64.zip
+unzip elasticsearch-8.10.0-windows-x86_64.zip
